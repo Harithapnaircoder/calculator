a53cd025000c9d701569b72a12fb72e89625f32d
SwiftScribeDev <harithapnair79@gmail.com>
2024-02-19 19:33:41 +0530

    Initial commit

---
+++ sample.js
@@ -0,0 +1,12 @@
+var screen = document.getElementById('screen');
+
+function btnClick(value) {
+    screen.value += value;
+}
+function clearscreen(){
+    screen.value = " ";
+}
+function result(){
+    var result=eval(screen.value)
+    screen.value=result;
+}
